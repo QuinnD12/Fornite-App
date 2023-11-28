e588086dddf8ab852ae5b6a812aadcdfe9df6a84
Quinn Davis <qdavis6106@stu.d214.org>
2023-11-28 10:49:01 -0600

    json decodetr

--- Fornite App/API Manager.swift
+++ Fornite App/API Manager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-func getUser(_ accountID: String, platform: String = "epic") async throws -> String {
+func getUser(_ accountID: String, platform: String = "epic") async throws -> FortniteID {
     let endpoint = "https://fortnite-api.com/v2/stats/br/v2/\(accountID)"
     guard let url = URL(string: endpoint) else {
         throw NetworkError.invalidURL
@@ -23,25 +23,77 @@ func getUser(_ accountID: String, platform: String = "epic") async throws -> Str
         throw NetworkError.invalidResponse
     }
     
-    //
-    /*do {
+    do {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         
-        return try decoder.decode(GitHubUser.self, from: data)
+        return try decoder.decode(FortniteID.self, from: data)
     } catch {
         throw NetworkError.invalidData
-    }*/
-    
-    return String(decoding: data, as: UTF8.self)
+    }
 }
-
-//struct ForniteUser: Codable {
-//
-//}
 
 enum NetworkError: Error {
     case invalidURL
     case invalidResponse
     case invalidData
+}
+
+struct FortniteID: Codable {
+    let result: Bool
+    let accountId: String
+}
+
+struct ForniteUser: Codable {
+    let status: Int
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let account: Account
+    let battlePass: BattlePass
+    let image: String
+    let stats: Stats
+}
+
+// MARK: - Account
+struct Account: Codable {
+    let id, name: String
+}
+
+// MARK: - BattlePass
+struct BattlePass: Codable {
+    let level, progress: Int
+}
+
+// MARK: - Stats
+struct Stats: Codable {
+    let all, keyboardMouse, gamepad, touch: All
+}
+
+// MARK: - All
+struct All: Codable {
+    let overall, solo: Overall?
+    let duo: Overall?
+    let trio: Overall?
+    let squad: Overall?
+    let ltm: Overall?
+}
+
+// MARK: - Overall
+struct Overall: Codable {
+    let score: Int
+    let scorePerMin, scorePerMatch: Double
+    let wins: Int
+    let top5, top12: Int?
+    let kills: Int
+    let killsPerMin, killsPerMatch: Double
+    let deaths: Int
+    let kd: Double
+    let matches: Int
+    let winRate: Double
+    let minutesPlayed, playersOutlived: Int
+    let lastModified: Date
+    let top3, top6, top10, top25: Int?
 }
